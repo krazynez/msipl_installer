ae6e83aa757321aef5a7f91d2c34832e7cae86c8
Brennen Murphy <brennenmurph@pm.me>
2024-04-03 13:20:52 -0400

    more possible fixes

--- msipl_installer.py
+++ msipl_installer.py
@@ -77,11 +77,12 @@ if is_windows:
 
 elif is_macos:
     diskID = f'/dev/{args.devname}'
-    subprocess.run(['diskutil', 'umountDisk', 'force', f'{diskID}'])
 
     def checkDiskType():
         block_size = subprocess.Popen([f"diskutil info {diskID} | awk '/Device Block Size/ {{print $4}}'"], shell=True, stdout=subprocess.PIPE)
         isBytesPerSecOk = bytesPerSector = int(block_size.stdout.read().decode())
+        subprocess.run(['diskutil', 'umountDisk', 'force', f'{diskID}'])
+        subprocess.run(['sync'])
         return True
 
     def openDisk():
